e48fa78c07c5488b0d98fbfc16e4a85c2041e57b
Yagya Adh <yagyaadhikari02@gmail.com>
2024-10-04 14:38:27 +0545

    create: home page create complete.

--- app/components/banner/MainBanner.tsx
+++ app/components/banner/MainBanner.tsx
@@ -7,21 +7,21 @@ const MainBanner = () => {
   return (
     <div className="relative mx-auto max-w-screen-2xl">
       <div className="absolute top-0">
-        <h1 className="text-white text-8xl font-extrabold max-w-screen-sm font-sonexFamily p-5">
+        <h1 className="text-white text-8xl font-extrabold max-w-screen-sm font-sonexFamily p-10">
           RISE WITH THE RIDING.
         </h1>
       </div>
       <Image src={MAINBANNERIMAGE} alt="main_banner_" className="z-10" />
-      <FootMainBanner />
+      <BannerFootSection />
     </div>
   );
 };
 
-const FootMainBanner = () => {
+const BannerFootSection = () => {
   return (
     <div className="flex flex-col items-end w-full absolute bottom-0">
-      <div className="flex justify-between items-end px-10 w-full">
-        <p className="text-white text-xl font-bold max-w-72">
+      <div className="flex justify-between items-end px-10">
+        <p className="text-white text-3xl font-bold max-w-screen-sm pe-40">
           YOUR SHOP FOR BICYCLES AND E-BIKES IN SWITZERLAND.
         </p>
         <Card
