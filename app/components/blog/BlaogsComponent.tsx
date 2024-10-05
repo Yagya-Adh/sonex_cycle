42392ceb136cac18e8d45ced0e52a0a3162c556e
Yagya Adh <yagyaadhikari02@gmail.com>
2024-10-05 12:57:58 +0545

    remove: default page, faq and event page remove

--- app/components/blog/BlaogsComponent.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-const BlaogsComponent = () => {
-  return <div>BlaogsComponent</div>;
-};
-
-export default BlaogsComponent;
--- app/components/faq/FAQsComponent.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FAQsComponent = () => {
-  return <div>FAQsComponent</div>;
-};
-
-export default FAQsComponent;
